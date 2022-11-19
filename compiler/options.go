898a06147e9659b41f0ed84c3a1792280d41464a
bichanna <nobu.bichanna@gmail.com>
2022-11-19 12:58:26 -0500

    Parse options and set them

--- compiler/options.go
+++ compiler/options.go
@@ -1,5 +1,16 @@
 package compiler
 
 var (
+	COptionsOutputFile               = ""
+	COptionsCompileOnly              = false
 	COptionsAggressiveErrorReporting = false
+	COptionsOptimize                 = false
+	COptionsDebug                    = false
+	COptionsStrip                    = false
+	COptionsMagic                    = false // Easter egg
+	COptionsWarnings                 = true
+	COptionsVersion                  = ""
+	COptionsWErrors                  = false
+	COptionsUnsafe                   = false
+	COptionsObjDmp                   = false
 )
