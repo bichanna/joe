25028e56ba6670493843e09de9f60a75ddf3f224
bichanna <nobu.bichanna@gmail.com>
2022-12-01 15:18:29 -0500

    Update lexer test

--- lexer_test.go
+++ lexer_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestNonAggressiveLexer(t *testing.T) {
+func Test1(t *testing.T) {
 	source := `(println "Hello World")
 	# Hello World
 	(def is-even? [num] (= 0 (% num 2)))`
@@ -15,5 +15,20 @@ func TestNonAggressiveLexer(t *testing.T) {
 	if len(tokens) != 21 {
 		// It's 21 because EOF token is also included.
 		t.Error("Invalid, got ", len(tokens))
+	}
+}
+
+func Test2(t *testing.T) {
+	source := `"Hello \n World"`
+
+	lexer := newLexer(source, "[input]", false)
+	tokens := lexer.tokenize()
+
+	if len(tokens) != 2 {
+		t.Error("Invalid, got", len(tokens))
+	}
+
+	if tokens[0].payload == "Hello \n World" {
+		t.Error("Invalid got \"", tokens[0].payload, "\"")
 	}
 }
